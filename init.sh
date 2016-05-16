6d1066fc56adad18a4eed6e3cda9c38bdce1fa4e
a2151257 <a2151257-2@yandex.ru>
2016-05-16 18:38:06 +0200

    box

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -x
 
-#sudo nginx -c $PWD/etc/nginx.conf
-#gunicorn -c $PWD/etc/hello.py hello:app
+sudo pip install --upgrade django gunicorn
+sudo /etc/init.d/mysql restart
+
+sudo nginx -c $PWD/etc/nginx.conf
+gunicorn -c $PWD/etc/hello.py hello:app
 
 cd ask
 gunicorn -c $PWD/../etc/ask.py ask.wsgi
