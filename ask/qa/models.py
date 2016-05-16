560c0863e0af278bfcf9ef86dc1adeeada0aa699
a2151257 <a2151257-2@yandex.ru>
2016-05-16 19:06:45 +0200

    now

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,14 +7,14 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField()
     author = models.ForeignKey(User, related_name="question_author")
     likes = models.ManyToManyField(User)
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
     
