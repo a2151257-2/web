fe096aa8f928e069235a1cf26c49d5838db62c87
a2151257 <a2151257-2@yandex.ru>
2016-05-16 19:11:35 +0200

    def

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, related_name="question_author")
     likes = models.ManyToManyField(User)
 
